6c22a90c72832044dc84091d3c5b2a1b7114be01
Dund33 <warior1112@gmail.com>
2021-12-16 23:21:48 +0100

    Fixes

--- src/kernel.rs
+++ src/kernel.rs
@@ -14,13 +14,16 @@ entry_point!(_krnl);
 
 #[no_mangle]
 fn _krnl(info: &'static mut BootInfo) -> !{
+
+    let mut screen = Screen{pos: 0};
+    let text: [u8; 13]= [
+        72 ,101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100, 33
+    ];
+    screen.print_str(&text);
     loop {}
 }
 
 #[panic_handler]
 fn panic(_: &PanicInfo) -> ! {
-    let mut screen = Screen{pos: 0};
-    let text = "Hello, world!".to_ascii_uppercase();
-    screen.print_str();
     loop {}
 }
