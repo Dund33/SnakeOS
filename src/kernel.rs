7174c4ff8211e8bcd9783894cc2d40758597a47f
Fatoumata Bocar <fatboc@gmail.com>
2021-12-17 16:35:36 +0100

    Added color support

--- src/kernel.rs
+++ src/kernel.rs
@@ -8,15 +8,17 @@ mod misc;
 use core::panic::PanicInfo;
 use core::alloc;
 use core::arch::asm;
-use crate::gfx::Screen;
+use crate::gfx::{Screen, Color, ColorData};
 use crate::misc::halt;
 
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start(){
     let mut screen = Screen::init();
+    let color = ColorData{front_color: Color::Magenta, back_color: Color::White};
     let text = b"Womnsze to peuzajom one";
-    screen.print_str(text);
+    screen.print_str(text, &color);
+    screen.print_str(text, &color);
     halt()
 }
 
