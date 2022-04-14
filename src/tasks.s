6692ad8bd4294316ed0f6d0e831ed6f24d9f03e8
Dund33 <warior1112@gmail.com>
2022-04-14 16:57:13 +0200

    Works with rust. Blocks on atomics. Need to fix the sleep function.

--- src/tasks.s
+++ src/tasks.s
@@ -1,121 +1,54 @@
-global pit_handler
 extern test
 extern kbrd_server
 extern test2
+extern swap
+global switch_to
+global pit_handler
 
-pit_handler:
-
-push eax
+section .text
+switch_to:
+pop eax; return address
+mov eax, [esp]
+mov ebx, [eax+40]
 push ebx
-mov eax, [process_num]
-imul eax, descriptor_size
-add eax, task_descriptors ;descriptor address at eax
-
-mov ebx, [esp + 16]
-mov [eax + 32], ebx ;eflags
-
-mov ebx, esp
-add ebx, 20
-mov [eax + 28], ebx ;esp
-
-mov ebx, [esp + 8]
-mov [eax + 24], ebx ;eip
-
-mov [eax], edi ;edi
-
-mov [eax + 4], esi ;esi
-
-mov [eax + 8], edx ;edx
-
-mov [eax + 12], ecx ;ecx
-
-pop ebx
-mov [eax + 16], ebx ;ebx
-
-pop ebx
-mov [eax + 20], ebx ;eax
-
-mov eax, [process_num]
-inc eax
-cmp eax, processes
-jl num_ok ;its okay lets proceed
-
-mov eax, 0 ;not ok, fix
-
-num_ok: ;now ok
-mov [process_num], eax
-imul eax, descriptor_size
-add eax, task_descriptors ;descriptor address at eax
-mov edi, [eax]
-mov esi, [eax + 4]
-mov edx, [eax + 8]
-mov ecx, [eax + 12]
-mov ebx, [eax + 16]
-
-push ebx
-
-mov ebx, [eax + 24]
-mov [new_ip], ebx
-
-pop ebx
-
-push eax
-mov eax, [eax + 32]
-cmp eax, 0
-je skipped_flags
-
-push eax
 popfd
-
-skipped_flags:
-pop eax
-
-mov esp, [eax + 28]
-
-mov eax, [eax + 20]
-
+mov ebx, [eax+32]
+mov [new_ip], ebx
+mov ebx, [eax+28]
+mov ecx, [eax+24]
+mov edx, [eax+20]
+mov esi, [eax+16]
+mov edi, [eax+12]
+mov ebp, [eax+8]
+mov esp, [eax+4]
+mov eax, [eax]
 push eax
 mov al, 0x20
 out 0x20, al
 pop eax
-
 sti
 jmp [new_ip]
 
+pit_handler:
+cli
+mov [tmp], eax
+push ebx
+push ecx
+push edx
+push esi
+push edi
+mov eax, esp
+add eax, 28
+push ebp
+push eax
+mov eax, [tmp]
+push eax
+call swap
+
 section .data
-process_num: db 0
-processes: equ 2
-descriptor_size equ 36
-task_descriptors:
-    dd  0
-    dd  0
-    dd  0
-    dd  0
-    dd  0
-    dd  0 
-    dd  0 ;instruction pointer
-    dd  0 ;stack pointer
-    dd  0 ;flags
-    ;-----------
-    dd  0
-    dd  0
-    dd  0
-    dd  0
-    dd  0
-    dd  0
-    dd  kbrd_server ;instruction pointer
-    dd  180000h ;stack pointer
-    dd  0 ;flags
-    ;-----------
-    dd  0
-    dd  0
-    dd  0
-    dd  0
-    dd  0
-    dd  0
-    dd  test ;instruction pointer
-    dd  200000h ;stack pointer
-    dd  0 ;flags
-
-new_ip: resb 4
-
+tmp:
+resb 4
+new_ip:
+resb 4
+new_esp:
+resb 4
